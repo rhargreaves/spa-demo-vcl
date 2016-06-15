15957cba528200006f4d48e34cf7971279703663
rhargreaves <robert.hargreaves@7digital.com>
2016-06-15 16:58:15 +0100

    Remove old script

--- test_vcl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VTC_FILE=$1
-VCL_FILE=$2
-CURRENT_DIR=$(pwd)
-
-sed -e '/# INSERT_VCL_HERE/ r '${VCL_FILE} -e 's/# INSERT_VCL_HERE//' ${VTC_FILE} > test.vtc.tmp
-
-docker run -i -v ${CURRENT_DIR}:/opt/varnish/tests roberthargreaves/varnishtest varnishtest /opt/varnish/tests/test.vtc.tmp
